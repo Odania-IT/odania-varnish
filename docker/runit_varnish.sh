637360a38f032a7a8955229be3981ef0c396e9d6
Mike Petersen <mike@odania-it.de>
2016-03-09 21:43:37 +0100

    Update Gemfile.lock

--- docker/runit_varnish.sh
+++ docker/runit_varnish.sh
@@ -8,4 +8,4 @@ bundle install
 rake varnish:generate
 
 echo "Starting varnish on port ${LISTEN_PORT} with config file ${VCL_CONFIG}"
-varnishd -j unix,user=varnish -F -a 0.0.0.0:$LISTEN_PORT -f $VCL_CONFIG
+exec varnishd -j unix,user=varnish -F -a 0.0.0.0:$LISTEN_PORT -f $VCL_CONFIG
